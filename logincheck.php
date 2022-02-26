28988335a5e25248492ead80c7548ea9bd29a53d
Jacob Grover <jacob@jacobgrover.com>
2022-02-26 12:53:20 -0500

    Small commit
    Just a very trivial update from yesterday(regarding error sent in POST)

--- logincheck.php
+++ logincheck.php
@@ -1,15 +1,15 @@
 <?php
 	session_start();
 	$dbloc = //
-	$dbuser = //
-	$dbpassword = //
-	$dbname = //
+	$dbuser = "//";//I couldn't get root to work correctly
+	$dbpassword = "//";
+	$dbname = "//";
 	$dbconnection = mysqli_connect($dbloc, $dbuser, $dbpassword, $dbname);
 
 	if(isset($_POST['username']) && isset($_POST['password'])) {
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-		$query = //
+		$query = "//";
 		$queryresult = mysqli_query($dbconnection, $query);
 		if(mysqli_num_rows($queryresult) === 1) {//identical operator, should only return one row because credentials are unique
 			$tablerow = mysqli_fetch_assoc($queryresult);//gives the table row back as an associative array, we need the variables for the user session
@@ -19,17 +19,17 @@
 				header("Location: userhome.php");
 			}
 			else {
-				header("Location: index.php?error=Invalid credentials!");
+				header("Location: index.php?error=yes");
 				exit();
 			}
 		}
 		else {
-			header("Location: index.php?error=Invalid credentials!");
+			header("Location: index.php?error=yes");
 			exit();
 		}
 	}
 	else {
-		header("Location: index.php?error=Invalid credentials!");
+		header("Location: index.php?error=yes");
 		exit();
 	}
 ?>
