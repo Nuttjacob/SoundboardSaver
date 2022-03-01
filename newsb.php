64234d57877c37a749a0948a59a391e8f5c4de0c
Nuttjacob <jacob@jacobgrover.com>
2022-03-01 15:12:45 -0500

    Sound submission form started
    There will be a place where you can submit your own sound to add to the sound library. They can be public or private.

--- newsb.php
+++ newsb.php
@@ -1,5 +1,6 @@
 <!--Jacob Grover, 2/27/22
 https://www.w3schools.com/sql/sql_distinct.asp
+https://developers.google.com/web/fundamentals/media/recording-audio
 -->
 <?php
 	session_start();
@@ -27,25 +28,32 @@ https://www.w3schools.com/sql/sql_distinct.asp
 				$totalcats = mysqli_query($dbconnection, "SELECT DISTINCT category FROM sounds;");//Gets all of the categories, should change this variable name
 				$catnum = mysqli_num_rows($totalcats);
 			?>
-			<fieldset id="newsoundfield">
-			<legend>Add an existing sound</legend>
-			<label for="categories">Categories</label><!--The categories themselves are loaded by PHP, then JS gets the sounds for the given category with more PHP-->
-			<select id="categories">
-				<?php
-				for($catite=0;$catite<=$catnum;$catite++) {
-					$catrow = mysqli_fetch_assoc($totalcats);//PHP has a "cursor" that goes along the query rows
-					$opvalue = $catrow['category'];
-					echo "<option value='{$opvalue}'>{$opvalue}</option>";
-				}
-				?>
-			</select>
-			<label for="sounds">Sounds</label>
-			<!--JS inserts the sounds for the category here -->
-			<select id="sounds">
-			</select>
-			<button type="button" id="playsoundsel">Play</button>
-			<button type="button" id="stopsoundsel">Stop</button>
-			<button type="button" id="addsoundbutton">Add</button>
+			<fieldset id="selsoundfield">
+				<legend>Add an existing sound</legend>
+				<label for="categories">Categories</label><!--The categories themselves are loaded by PHP, then JS gets the sounds for the given category with more PHP-->
+				<select id="categories">
+					<?php
+					for($catite=0;$catite<=$catnum;$catite++) {
+						$catrow = mysqli_fetch_assoc($totalcats);//PHP has a "cursor" that goes along the query rows
+						$opvalue = $catrow['category'];
+						echo "<option value='{$opvalue}'>{$opvalue}</option>";
+					}
+					?>
+				</select>
+				<label for="sounds">Sounds</label>
+				<!--JS inserts the sounds for the category here -->
+				<select id="sounds">
+				</select>
+				<div id="selcontrols">
+					<button type="button" id="playsoundsel">Play</button>
+					<button type="button" id="stopsoundsel">Stop</button>
+					<button type="button" id="addsoundbutton">Add</button>
+				</div>
+			</fieldset>
+			<fieldset id="upsoundfield">
+			<legend>Upload a new sound</legend>
+			<input type="file" id="filesel" name="file" accept="audio/mp3, audio/wav" capture><!-- get capture working -->
+			<input type="submit">
 			</fieldset>
 		</main>
 	</body>
